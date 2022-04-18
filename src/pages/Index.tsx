505a8a04ce2bf050554868316221eb268aa35e87
Cupid Valentine <westerngod@126.com>
2022-04-18 16:44:10 +0800

    add header

---
+++ src/pages/Index.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import Clients from '../widgets/Clients'
+import Contact from '../widgets/Contact'
+import Footer from '../widgets/Footer'
+import Header from '../widgets/Header'
+import Hero from '../widgets/Hero'
+import HowItWorks from '../widgets/HowitWorks'
+import Solutions from '../widgets/Solutions'
+
+const Index = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <Header></Header>
+      <main className="mt-20">
+        <Hero></Hero>
+        <section className="md:p-20">
+          <Solutions />
+        </section>
+        <section className="mt-20">
+          <HowItWorks />
+        </section>
+        <section className="mt-20 md:w-3/5 mx-auto">
+          <Clients />
+        </section>
+        <section className="mt-20">
+          <Contact />
+        </section>
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+export default Index
