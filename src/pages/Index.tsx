eb4868cf8305a1523ab7d3eec6b139d4f5ec66e1
Cupid Valentine <westerngod@126.com>
2022-04-18 16:46:54 +0800

    add hero

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,9 +1,13 @@
 import Header from '../widgets/Header'
+import Hero from '../widgets/Hero'
 
 const Index = () => {
   return (
     <div className="container mx-auto p-4">
       <Header></Header>
+      <main className="mt-20">
+        <Hero></Hero>
+      </main>
     </div>
   )
 }
---
+++ src/widgets/Hero.tsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import Video from '../assets/production_ID_4167404.mp4'
+const Hero = () => {
+  return (
+    <div className="grid justify-items-center gap-8 pb-28 relative">
+      <p className="text-4xl md:text-6xl font-black text-center leading-normal md:leading-normal">
+        this is content
+      </p>
+      <p className="text-xl text-gray-700 md:w-1/2 text-center">
+        this is sub content
+      </p>
+      <div>
+        <button className="rounded bg-blue-500 text-base text-white py-3 px-8">
+          立即试用
+        </button>
+        <button className="rounded bg-gray-900 text-base text-white py-3 px-8 ml-3">
+          了解更多
+        </button>
+      </div>
+
+      <div className="relative w-full">
+        <svg
+          width="256"
+          height="256"
+          viewBox="0 0 256 256"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute right-0 -z-10"
+        >
+          <circle cx="128" cy="128" r="128" fill="url(#paint0_linear_0_22)" />
+          <defs>
+            <linearGradient
+              id="paint0_linear_0_22"
+              x1="0"
+              y1="0"
+              x2="0"
+              y2="256"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stopColor="white" />
+              <stop offset="0.774017" stopColor="#EAEAEA" />
+              <stop offset="1" stopColor="#DFDFDF" />
+            </linearGradient>
+          </defs>
+        </svg>
+      </div>
+
+      <div className="relative grid justify-items-center">
+        {/* 自定义长宽 */}
+        <video
+          src={Video}
+          controls
+          className="w-[768px] h-[432px] object-cover object-top rounded"
+        ></video>
+
+        <div className="flex absolute rounded-full bg-white -bottom-5 px-5 py-4 drop-shadow-xl">
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="mr-3"
+          >
+            <circle cx="12" cy="12" r="11" stroke="#999999" strokeWidth="2" />
+            <path d="M10 17L16 12L10 7V17Z" fill="#999999" />
+          </svg>
+          查看2分钟演示视频
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
